f3fd9ee07206e990ec448636c728fa951f3d5967
Johannes Coetsee <johannes.coetsee@ktu.edu>
2024-11-23 02:54:15 +0200

    SCRUM-14 and SCRUM-17

--- TravelAssistant/travelapis/management/commands/populate_locations.py
+++ TravelAssistant/travelapis/management/commands/populate_locations.py
@@ -4,7 +4,7 @@ from travelapis.models import Locations
 import environ
 import requests
 
-def api_request(category):
+def places_api_request(category):
     env = environ.Env()
     environ.Env.read_env()
     api_key = env("PLACES_KEY")
@@ -17,7 +17,7 @@ def api_request(category):
     params = {
         "categories": category,
         "filter": filter_value,
-        "limit": 50,
+        "limit": 300,
         "apiKey": api_key
     }
 
@@ -36,6 +36,89 @@ def api_request(category):
     except requests.exceptions.RequestException as e:
         return {"error": f"Failed to connect to Geoapify API: {str(e)}"}
 
+def place_details(body, key):
+    p_id = body["id"]
+
+    details_url = f"https://places.googleapis.com/v1/places/{p_id}"
+    details_parameters = {
+        "key": key,
+        "fields": "regularOpeningHours,rating,priceRange,priceLevel,allowsDogs,goodForGroups,goodForChildren"
+    }
+
+    try:
+        response = requests.get(details_url, params=details_parameters)
+
+        if response.status_code == 200:
+            data = response.json()
+            return {
+                "rating": data.get("rating"),
+                "hours": data.get("regularOpeningHours", {}).get("weekdayDescriptions"),
+                "priceLevel": data.get("priceLevel"),
+                "priceMin": data.get("priceRange", {}).get("startPrice", {}).get("units"),
+                "priceMax": data.get("priceRange", {}).get("endPrice", {}).get("units"),
+                "allowsDogs": data.get("allowsDogs"),
+                "goodForGroups": data.get("goodForGroups"),
+                "goodForChildren": data.get("goodForChildren")
+            }
+        else:
+            print(f"No details Found for place ID: {response.text}")
+            return None
+
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Failed to connect to Google API: {str(e)}"}
+
+def place_retrieve(lat, lon):
+    env = environ.Env()
+    environ.Env.read_env()
+    api_key = env("GOOGLE_KEY")
+
+    if not api_key:
+        raise ValueError("Missing GOOGLE API key from environment variables")
+
+    nearby_url = "https://places.googleapis.com/v1/places:searchNearby"
+    nearby_headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": f"{api_key}",
+        "X-Goog-FieldMask": "places.id,places.displayName"
+    }
+    nearby_data = {
+        "maxResultCount": 1,
+        "locationRestriction": {
+            "circle": {
+                "center": {
+                    "latitude": lat,
+                    "longitude": lon
+                },
+                "radius": 25
+            }
+        }
+    }
+
+    try:
+        response = requests.post(nearby_url, json=nearby_data, headers=nearby_headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            places = data.get("places", [])
+            if isinstance(places, list) and len(places) > 0:
+                place = places[0]
+            else:
+                print("No places found in the response.")
+                return None
+            return place_details(place, api_key)
+        elif response.status_code == 404:
+            print("No places found for lat and long coordinates")
+            return None
+        elif response.status_code == 400:
+            print(f"Bad Request. {response.text}")
+            return None
+        else:
+            print(f"Unexpected error occurred: {response.status_code}")
+            return None
+
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Failed to connect to Google API: {str(e)}"}
+
 class Command(BaseCommand):
     help = 'Script for populating locations'
     def handle(self, *args, **options):
@@ -44,7 +127,7 @@ class Command(BaseCommand):
             f.close()
 
         for category in categories:
-            response = api_request(category)
+            response = places_api_request(category)
             if "error" in response:
                 self.stdout.write(
                     self.style.ERROR(f"Error fetching data for category '{category}': {response['error']}"))
@@ -53,15 +136,36 @@ class Command(BaseCommand):
             features = response.get("features", [])
             for feature in features:
                 properties = feature.get("properties", {})
+                if not Locations.objects.check_unique(properties.get("name"), properties.get("formatted")):
+                    continue
+                details = place_retrieve(properties.get("lat"), properties.get("lon"))
                 try:
-                    Locations.objects.create(
-                        id=properties.get("place_id"),
-                        name=properties.get("name"),
-                        lon=properties.get("lon"),
-                        lat=properties.get("lat"),
-                        address=properties.get("formatted"),
-                        categories=properties.get("categories", [])
-                    )
+                    if details:
+                        Locations.objects.create(
+                            id=properties.get("place_id"),
+                            name=properties.get("name"),
+                            lon=properties.get("lon"),
+                            lat=properties.get("lat"),
+                            address=properties.get("formatted"),
+                            categories=properties.get("categories", []),
+                            rating = details.get("rating"),
+                            hours = details.get("hours"),
+                            priceLevel = details.get("priceLevel"),
+                            priceMin = details.get("priceMin"),
+                            priceMax = details.get("priceMax"),
+                            allowsDogs = details.get("allowsDogs"),
+                            goodForGroups = details.get("goodForGroups"),
+                            goodForChildren = details.get("goodForChildren")
+                        )
+                    else:
+                        Locations.objects.create(
+                            id=properties.get("place_id"),
+                            name=properties.get("name"),
+                            lon=properties.get("lon"),
+                            lat=properties.get("lat"),
+                            address=properties.get("formatted"),
+                            categories=properties.get("categories", [])
+                        )
                 except IntegrityError as e:
                     print(f"Location {properties.get('name')} already exists")
             self.stdout.write(self.style.SUCCESS(f"Successfully populated data for category: {category}"))
--- TravelAssistant/travelapis/views.py
+++ TravelAssistant/travelapis/views.py
@@ -1,12 +1,27 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from . import models
+from .models import Locations
 
 
 @api_view(['GET'])
 def locations_list(request):
-    locations = list(models.Locations.objects.all().values())
+    locations = list(Locations.objects.get_all())
     return Response(locations, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def locations_filter_search(request):
+    locations = Locations.objects.filter_search(request.data)
+    if len(locations) > 0:
+        return Response(locations[0:100], status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['PUT'])
+def update_location_priority(request):
+    location = Locations.objects.update_priority(request.data)
+    if location:
+        return Response(location, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
